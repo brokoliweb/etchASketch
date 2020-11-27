2cc654f0a4db37c8e549e44c3ab76216988cfa2d
Baris Bilgin <brokoliweb@gmail.com>
2020-11-27 15:41:40 +0300

    add changeColor at click and remove padding from grid-item

--- main.js
+++ main.js
@@ -1,36 +1,46 @@
 const container = document.getElementById("container");
-const slider = document.getElementById('grid-width');
-const sliderValue = document.getElementById('range-value');
+const slider = document.getElementById("grid-width");
+const sliderValue = document.getElementById("range-value");
 
 sliderValue.innerText = slider.value;
 
-slider.addEventListener('mousemove', changeValue);
-slider.addEventListener('mouseup', drawNewGrid);
+slider.addEventListener("mousemove", changeValue);
+slider.addEventListener("mouseup", drawNewGrid);
+
 
 function changeValue(e) {
-    
-    sliderValue.innerText = slider.value;
-    
+  sliderValue.innerText = slider.value;
 }
-
 
 function makeRows(rows) {
-    container.style.setProperty('grid-template-rows', `repeat(${rows}, 1fr)`);
-    container.style.setProperty('grid-template-columns', `repeat(${rows}, 1fr)`);
-    
+  container.style.setProperty("grid-template-rows", `repeat(${rows}, 1fr)`);
+  container.style.setProperty("grid-template-columns", `repeat(${rows}, 1fr)`);
 
-
-  for (c = 0; c < (rows * rows); c++) {
+  for (c = 0; c < rows * rows; c++) {
     let cell = document.createElement("div");
     container.appendChild(cell).className = "grid-item";
-  };
-};
-
-function drawNewGrid(e) {
-    console.log(e);
-    container.innerText = "";
-    makeRows(slider.value);
-    
+  }
 }
 
-makeRows(slider.value)
+function drawNewGrid(e) {
+  console.log(e);
+  container.innerText = "";
+  startup();
+}
+
+function changeColor(e) {
+    console.log(e);
+    e.target.style.backgroundColor = "red";
+}
+
+function startup() {
+    
+    makeRows(slider.value);
+
+const cells = document.querySelectorAll(".grid-item");
+console.log(cells);
+
+cells.forEach(cell=> cell.addEventListener('click', changeColor));
+}
+
+startup();
